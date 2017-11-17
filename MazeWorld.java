16eb7c42de8f48348ae421de3d5867a1174e09c3
Patrick Jimenez <pjimenezgicko@gmail.com>
2017-11-17 16:11:34 -0500

    saved

--- MazeWorld.java
+++ MazeWorld.java
@@ -3,3 +3,13 @@ import tester.*;
 import javalib.impworld.*;
 import java.awt.Color;
 import javalib.worldimages.*;
+
+class MazeWorld {
+  // Size of maze
+  static final int MAZE_SIZE = 64;
+  
+  // Maze represented as a 2D Arraylist of Nodes
+  ArrayList<ArrayList<Node>> maze;
+  
+  
+}
