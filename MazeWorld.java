74e3b39714b0fca6ae57d8d6e07071f39ad94d09
Patrick Jimenez <pjimenezgicko@gmail.com>
2017-11-21 10:31:14 -0500

    initHash, node position redundency

--- MazeWorld.java
+++ MazeWorld.java
@@ -8,38 +8,69 @@ import javalib.worldimages.*;
 
 class MazeWorld {
   // Size of maze
-  static final int MAZE_SIZE = 64;
-  
+  static final int           MAZE_SIZE = 64;
+
+  // Size of node
+  static final int           NODE_SIZE = 10;
+
   // Maze represented as a 2D Arraylist of Nodes
   ArrayList<ArrayList<Node>> maze;
   
+  // Maze as a 1D Arraylist
+  ArrayList<Node> maze2;
+
   ArrayList<ArrayList<Edge>> edges;
-  
-  HashMap<Posn, Node> hash;
-  
+
+  HashMap<Posn, Node>        hash;
+
+  // image for the game
+  WorldImage                 image     = new EmptyImage();
+  // temp for testing
+  WorldScene                 scene     = new WorldScene(MAZE_SIZE * 100, MAZE_SIZE * 100);
+
   MazeWorld() {
-    
+
   }
-  
-  // 
+
+  public WorldScene makeScene() {
+    WorldScene w = new WorldScene(this.MAZE_SIZE * 100, MazeWorld.MAZE_SIZE);
+    
+    // Render the Cells
+    for (Node n : this.maze2) {
+      w.placeImageXY(n.drawAt(this.image), n.x * MazeWorld.NODE_SIZE, n.y * MazeWorld.NODE_SIZE);
+    }
+
+    return w;
+  }
+
+  //
   void initEmptyMaze() {
     this.maze = new ArrayList<ArrayList<Node>>(MAZE_SIZE);
     for (int i = 0; i < MAZE_SIZE; i++) {
       ArrayList<Node> temp = new ArrayList<Node>(MAZE_SIZE);
       for (int j = 0; j < MAZE_SIZE; j++) {
-        temp.set(j, new Node(new Posn(i, j));
+        temp.set(j, new Node(new Posn(i, j)));
       }
       this.maze.set(i, temp);
     }
     this.initEdges();
   }
-  
-  
+
   void initEdges() {
-    
+
   }
-  
+
   void initHash() {
+    ArrayList<Node> tempMaze = new ArrayList<Node>(0);
+    HashMap<Posn, Node> tempHash = new HashMap<Posn, Node>(0);
+    for (ArrayList<Node> arr : this.maze) {
+      for (Node n : arr) {
+        tempMaze.add(n);
+        tempHash.put(n.xy, n);
+      }
+    }
     
+    this.maze2 = tempMaze;
+    this.hash = tempHash;
   }
 }
--- Node.java
+++ Node.java
@@ -9,16 +9,22 @@ import javalib.worldimages.*;
 class Node {
   Posn xy;
   
+  int x;
+  int y;
   ArrayList<Edge> out;
   
   
   Node(Posn xy) {
     this.xy = xy;
+    this.x = xy.x;
+    this.y = xy.y;
     this.out = new ArrayList<Edge>(0);
   }
   
   Node(Posn xy, ArrayList<Edge> out) {
     this.xy = xy;
+    this.x = xy.x;
+    this.y = xy.y;
     this.out = out;
   }
 }
